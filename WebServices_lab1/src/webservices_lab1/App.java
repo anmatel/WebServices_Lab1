69b72e23f5889ddef731ee6e298ea0c0b37b1672
makstud <mak.anastasiya511@gmail.com>
2019-03-10 21:40:39 +0300

    Added classes

---
+++ WebServices_lab1/src/webservices_lab1/App.java
@@ -0,0 +1,10 @@
+package webservices_lab1;
+
+import javax.xml.ws.Endpoint;
+
+public class App {
+     public static void main(String[] args) {         
+         String url = "http://0.0.0.0:1111/PersonService";
+         Endpoint.publish(url, new PersonWebService()); 
+     }
+}
