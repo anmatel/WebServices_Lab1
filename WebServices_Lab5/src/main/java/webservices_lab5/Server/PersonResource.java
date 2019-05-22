502b7cc1e611bc4ed15922ee4f125b9e5922d8ec
makstud <mak.anastasiya511@gmail.com>
2019-05-22 22:08:34 +0300

    Implemented endpoint to upload file

--- WebServices_Lab5/src/main/java/webservices_lab5/Server/PersonResource.java
+++ WebServices_Lab5/src/main/java/webservices_lab5/Server/PersonResource.java
@@ -1,9 +1,18 @@
 package webservices_lab5.Server;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 import java.util.Date;
 import java.util.List;
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 
 @Path("/persons")
 @Produces({MediaType.APPLICATION_JSON})
@@ -30,7 +39,7 @@ public class PersonResource {
         int id = new PostgreSQLDAO().createPerson(name, surname, dateOfBirth, sex);
         return id;
     }
-    
+
     @POST
     @Path("/update")
     public String updatePerson(
@@ -42,11 +51,32 @@ public class PersonResource {
         String status = new PostgreSQLDAO().updatePerson(id, name, surname, dateOfBirth, sex);
         return status;
     }
-    
+
     @DELETE
     @Path("/delete")
     public String deletePerson(@QueryParam("id") int id) {
         String status = new PostgreSQLDAO().deletePerson(id);
         return status;
+    }
+
+    @POST
+    @Path("/uploadFile")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(
+            @FormDataParam("file") InputStream fileInputStream,
+            @FormDataParam("file") FormDataContentDisposition contentDispositionHeader) {
+        String filePath = "D:\\tmp\\".concat(contentDispositionHeader.getFileName());
+
+        try (FileOutputStream output = new FileOutputStream(new File(filePath))) {
+            byte[] buffer = new byte[fileInputStream.available()];
+            fileInputStream.read(buffer, 0, buffer.length);
+            output.write(buffer, 0, buffer.length);
+            fileInputStream.close();    
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        String message = "File saved to server location : " + filePath;
+        return Response.status(200).entity(message).build();
     }
 }
